d59c4885b289f61ab81e65b1f94521a972225de3
Jayakrishnan VR <jayakrishnanvrjk@gmail.com>
2020-12-26 23:09:53 +0530

    First Commit

---
+++ demo.js
@@ -0,0 +1,33 @@
+var b = "smoote";
+console.log(b);
+
+document.getElementById("heading").innerHTML = "Ada mwonae"
+//var age = prompt("poda myre")
+//alert("enthonnedey")
+var c = 50;
+var c = 100;
+
+//Function
+
+function greetings(){
+    alert("onj podaave");
+}
+
+//greetings();
+
+function finding(){
+    let word = prompt("enthelum okke parayadey-")
+    alert(word + " inganokke parayaavo??")
+}
+
+//finding ();
+
+function addNumber(num1, num2){
+    console.log(num1+num2)
+}
+
+addNumber(1,2)
+addNumber("enth",6)
+addNumber("Enth", " myranaada??")
+
+console.log("test")
